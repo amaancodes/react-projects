45c43bfdaf0698fa39468b9bc99ef600fb8d0294
Amaan <75938628+amaancodes@users.noreply.github.com>
2022-09-08 12:08:19 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>This is my first react project</h1>
+      <h2>React sounds interesting. Lets see what I can build</h2>
+    </div>
+  );
+}
